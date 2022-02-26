b82f6acdeaf04ab18fdd22aebae87acd4f46a64b
Cruz <cruz.tag@gmail.com>
2022-02-25 21:52:10 -0600

    end of the part1

--- notes-app-frontend/src/index.js
+++ notes-app-frontend/src/index.js
@@ -1,74 +1,277 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-// desestructuracion//
-// const Hello = ({ name, age }) => {
-//   const bornYear = () => {
-//     const yearNow = new Date().getFullYear();
-//     return yearNow - age;
+////////////////////////////Complex state////////////////////////////
+
+// const App = () => {
+//   const [left, setLeft] = useState(0);
+//   const [right, setRight] = useState(0);
+
+//   return (
+//     <div>
+//       {left}
+//       <button onClick={() => setLeft(left + 1)}>left</button>
+//       <button onClick={() => setRight(right + 1)}>right</button>
+//       {right}
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   const [clicks, setClicks] = useState({
+//     left: 0,
+//     right: 0,
+//   });
+
+//   //   const handleLeftClick = () => {
+//   //     const newClicks = {
+//   //       left: clicks.left + 1,
+//   //       right: clicks.right,
+//   //     };
+//   //     setClicks(newClicks);
+//   //   };
+
+//   //   const handleRightClick = () => {
+//   //     const newClicks = {
+//   //       left: clicks.left,
+//   //       right: clicks.right + 1,
+//   //     };
+//   //     setClicks(newClicks);
+//   //   };
+
+//   //   spread sintaxis
+
+//   //   const handleLeftClick = () => {
+//   //     const newClicks = {
+//   //       ...clicks,
+//   //       left: clicks.left + 1,
+//   //     };
+//   //     setClicks(newClicks);
+//   //   };
+
+//   //   const handleRightClick = () => {
+//   //     const newClicks = {
+//   //       ...clicks,
+//   //       right: clicks.right + 1,
+//   //     };
+//   //     setClicks(newClicks);
+//   //   };
+
+//   //   without assign the object to a variable
+
+//   const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 });
+
+//   const handleRightClick = () =>
+//     setClicks({ ...clicks, right: clicks.right + 1 });
+
+//   return (
+//     <div>
+//       {clicks.left}
+//       <button onClick={handleLeftClick}>left</button>
+//       <button onClick={handleRightClick}>right</button>
+//       {clicks.right}
+//     </div>
+//   );
+// };
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+////////////////////////////Handling arrays////////////////////////////
+
+// const App = () => {
+//   const [left, setLeft] = useState(0);
+//   const [right, setRight] = useState(0);
+//   const [allClicks, setAll] = useState([]);
+
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'));
+//     setLeft(left + 1);
+//   };
+
+//   const handleRightClick = () => {
+//     setAll(allClicks.concat('R'));
+//     setRight(right + 1);
 //   };
 
 //   return (
 //     <div>
-//       <p>
-//         Hello {name}, you are {age} years old
-//       </p>
-//       <p>So you were probably born in {bornYear()}</p>
+//       {left}
+//       <button onClick={handleLeftClick}>left</button>
+//       <button onClick={handleRightClick}>right</button>
+//       {right}
+//       {/* We call the join method on the allClicks array that joins all
+//       the items into a single string, separated by the string passed as
+//       the function parameter, which in our case is an empty space. */}
+//       <p>{allClicks.join(' ')}</p>
 //     </div>
 //   );
 // };
 
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+////////////////////////////Conditional rendering////////////////////////////
+
+// const History = (props) => {
+//   if (props.allClicks.length === 0) {
+//     return <div>the app is used by pressing the buttons</div>;
+//   }
+//   return <div>button press history: {props.allClicks.join(' ')}</div>;
+// };
+
+// const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
 // const App = () => {
-//   const name = 'Peter';
-//   const age = 10;
+//   const [left, setLeft] = useState(0);
+//   const [right, setRight] = useState(0);
+//   const [allClicks, setAll] = useState([]);
+
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'));
+//     setLeft(left + 1);
+//   };
+
+//   const handleRightClick = () => {
+//     setAll(allClicks.concat('R'));
+//     setRight(right + 1);
+//   };
 
 //   return (
 //     <div>
-//       <h1>Greetings</h1>
-//       <Hello name="Maya" age={26 + 10} />
-//       <Hello name={name} age={age} />
+//       {left}
+//       <Button onClick={handleLeftClick} text="left" />
+//       <Button onClick={handleRightClick} text="right" />
+//       {right}
+//       <History allClicks={allClicks} />
 //     </div>
 //   );
 // };
 
-// estado //
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+/////////////////////Function that returns a function/////////////////////////////////
+
 // const App = () => {
-//   const [counter, setCounter] = useState(0);
+//   const [value, setValue] = useState(10);
 
-//   setTimeout(() => setCounter(counter + 1), 1000);
+//   const hello = () => {
+//     const handler = () => console.log('hello world');
+//     return handler;
+//   };
 
-//   console.log('rendering...', counter);
-
-//   return <div>{counter}</div>;
+//   return (
+//     <div>
+//       {value}
+//       {/* ATTENTION HERE  */}
+//       <button onClick={hello()}>button</button>
+//     </div>
+//   );
 // };
 
-const Display = ({ counter }) => <div>{counter}</div>;
+// const App = () => {
+//   const [value, setValue] = useState(10);
 
-const Button = ({ handleClick, text }) => {
-  return <button onClick={handleClick}>{text}</button>;
-};
+//   // const hello = (who) => {
+//   //   const handler = () => {
+//   //     console.log('hello', who);
+//   //   };
+//   //   return handler;
+//   // };
 
-// const Button = (props) => {
+//   // const hello = (who) => {
+//   //   return () => {
+//   //     console.log('hello', who);
+//   //   };
+//   // };
+
+//   // const hello = (who) =>
+//   // () => {
+//   //   console.log('hello', who)
+//   // }
+
+//   const hello = (who) => () => {
+//     console.log('hello', who);
+//   };
+
 //   return (
-//     <button onClick={props.handleClick}>
-//       {props.text}
-//     </button>
-//   )
-// }
+//     <div>
+//       {value}
+//       <button onClick={hello('world')}>button</button>
+//       <button onClick={hello('react')}>button</button>
+//       <button onClick={hello('function')}>button</button>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   const [value, setValue] = useState(10);
+
+//   const setToValue = (newValue) => () => {
+//     setValue(newValue);
+//   };
+
+//   return (
+//     <div>
+//       {value}
+//       <button onClick={setToValue(1000)}>thousand</button>
+//       <button onClick={setToValue(0)}>reset</button>
+//       <button onClick={setToValue(value + 1)}>increment</button>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   const [value, setValue] = useState(10);
+
+//   const setToValue = (newValue) => {
+//     setValue(newValue);
+//   };
+
+//   return (
+//     <div>
+//       {value}
+//       <button onClick={() => setToValue(1000)}>thousand</button>
+//       <button onClick={() => setToValue(0)}>reset</button>
+//       <button onClick={() => setToValue(value + 1)}>increment</button>
+//     </div>
+//   );
+// };
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+/////////////////Passing Event Handlers to Child Components/////////////////////
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [value, setValue] = useState(10);
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
-
+  const setToValue = (newValue) => {
+    setValue(newValue);
+  };
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="plus" />
-      <Button handleClick={setToZero} text="zero" />
-      <Button handleClick={decreaseByOne} text="minus" />
+      {value}
+      {/* se pasan tal cual al componente */}
+      <Button
+        handleClick={() => {
+          setToValue(1000);
+        }}
+        text="thousand"
+      />
+      <Button
+        handleClick={() => {
+          setToValue(0);
+        }}
+        text="reset"
+      />
+      <Button
+        handleClick={() => {
+          setToValue(value + 1);
+        }}
+        text="increment"
+      />
     </div>
   );
 };
