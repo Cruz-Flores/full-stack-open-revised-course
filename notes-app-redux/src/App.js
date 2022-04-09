7e7f843930847c81374b8578a288c2f49211d7c2
Cruz <cruz.tag@gmail.com>
2022-04-09 01:25:17 -0500

    changed name of file Newnotes for NewNotes

--- notes-app-redux/src/App.js
+++ notes-app-redux/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Notes } from './components/Notes';
-import { NewNote } from './components/Newnote';
+import { NewNote } from './components/NewNote';
 import { VisibilityFilter } from './components/VisibilityFilter';
 import noteService from './services/notes';
 import { initializeNotes } from './reducers/noteReducer';
